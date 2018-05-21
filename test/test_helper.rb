10414d189441342fcbae30ed05640cebf11d10cd
h-kubo <z13026hk@aiit.ac.jp>
2018-05-21 09:03:24 +0000

    Finish user edit, update, index, and destroy actions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,13 +3,30 @@ require 'rails/test_help'
 require "minitest/reporters"
 Minitest::Reporters.use!
 
+#-------------------------------------------------------
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
+  
+  #---------------------
   def is_logged_in?
     !session[:user_id].nil?
   end
 
+  #---------------------
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
   # Add more helper methods to be used by all tests here...
+end
+
+#-------------------------------------------------------
+class ActionDispatch::IntegrationTest
+
+  # テストユーザーとしてログインする
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
 end
