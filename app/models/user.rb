ac4beb226103a4c220becdfdd741f5875f04a35e
h-kubo <z13026hk@aiit.ac.jp>
2018-06-04 06:23:09 +0000

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,19 @@
 class User < ApplicationRecord
+#--------------------------
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+                                  
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+                                   
+  has_many :following, through: :active_relationships, source: :followed
+
+  has_many :followers, through: :passive_relationships, source: :follower
+
+#-------------------------- 
   before_save { self.email = email.downcase }
   #validates :name, presence: true
   #validates :email, presence: true
@@ -19,6 +33,27 @@ class User < ApplicationRecord
   end
   
   def feed
-    Micropost.where("user_id = ?", id)
+    # Micropost.where("user_id = ?", id)
+    # Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+    # Micropost.where("user_id IN (:following_ids) OR user_id = :user_id", following_ids: following_ids, user_id: id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+    
   end
+  
+  # ユーザーをフォローする
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end  
 end
